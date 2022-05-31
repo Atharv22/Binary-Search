d9c9b9e3d81bcfcb9c4579f5f95b6a0418a885d7
Atharv22 <63144780+Atharv22@users.noreply.github.com>
2022-05-31 22:15:07 +0530

    Create binarySearch.cpp

---
+++ binarySearch.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int s, int n, int key){
+    int e=n;
+    while (s<=n){
+        int mid = (s+e)/2;
+        if (arr[mid]==key)
+            return mid;
+        else if(arr[mid]>key){
+            e=mid-1;
+            return binarySearch(arr, s, e, key);
+        }
+        else{
+            s=mid+1;
+            return binarySearch(arr, s, e, key);
+        }
+    }
+    return -1;
+}
+int main(){
+    int key, n;
+    int s=0;
+    cout<<"Enter the number of elements:";
+    cin>>n;
+    cout<<"Enter your Array in sorted order\n";
+    int arr[n];
+    for (int i=0; i<n; i++)
+        cin>>arr[i];
+    cout<<"Enter the number to be searched: \n";
+    cin>>key;
+    cout<<binarySearch(arr, s, n, key) + 1 <<endl;
+    return 0;
+
+}
